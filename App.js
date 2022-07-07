f761ffbb605817b1e4c34cff2330cb91806edddb
Fernando Caride <fcaride@gmail.com>
2022-07-07 10:08:08 +0200

    andando cast

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { CastButton, useRemoteMediaClient } from "react-native-google-cast";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app! kkk</Text>
       <StatusBar style="auto" />
+      <MyComponent />
     </View>
   );
 }
@@ -13,8 +16,29 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
+
+function MyComponent() {
+  // This will automatically rerender when client is connected to a device
+  // (after pressing the button that's rendered below)
+  const client = useRemoteMediaClient();
+
+  if (client) {
+    // Send the media to your Cast device as soon as we connect to a device
+    // (though you'll probably want to call this later once user clicks on a video or something)
+    client.loadMedia({
+      mediaInfo: {
+        contentUrl:
+          "https://bafybeiermzzq7elgjtynvtv5t2ynkz3xvubi726hngo6bi3dgbpph5ueku.ipfs.dweb.link/Los%20Simpsons/Temporada%2006/Los%20Simpsons%206x01%20-%20El%20diab%C3%B3lico%20Bart.mp4",
+      },
+    });
+  }
+
+  // This will render native Cast button.
+  // When a user presses it, a Cast dialog will prompt them to select a Cast device to connect to.
+  return <CastButton style={{ width: 24, height: 24, tintColor: "black" }} />;
+}
