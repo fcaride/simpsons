895218d42782f0cae3cee03eddf7ab937239c71e
Fernando Caride <fcaride@gmail.com>
2023-07-14 09:53:36 +0200

    test

--- Home.js
+++ Home.js
@@ -31,7 +31,7 @@ export function Home() {
         style={styles.inputSearch}
         value={query}
         onChangeText={setQuery}
-        placeholder="Buscar episodio"
+        placeholder="Buscar episodioaaaa"
       />
       <SectionList
         sections={query ? episodes : sectionsEpisodes}
