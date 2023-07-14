29a045994783f715a1f11a83bd69ec2f03c18f18
Fernando Caride <fcaride@gmail.com>
2023-07-14 12:55:07 +0200

    fix crash

--- App.js
+++ App.js
@@ -2,7 +2,7 @@
 
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import * as React from "react";
+import { useEffect } from "react";
 import { TouchableOpacity } from "react-native";
 import {
   CastButton,
